6ad09dcafd617356f201a5688d71a023411afe91
Yaduo Liu <yaduol@hotmail.com>
2020-02-04 09:15:48 -0800

    css 替换为 less

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
-import './index.css';
-import 'antd/dist/antd.css'; 
+import './index.less';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,40 +1,29 @@
 import React from 'react';
-import logo from '../assets/logo.svg';
-import { DatePicker } from 'antd';
+import "./Home.less"
+import { MainLayout } from '../components'
 
 export const Home: React.FC = () => {
   return (
-   <div className="home-page">
-      
-      <DatePicker />
-      
-      <header>
-        header
-      </header>
-
-      <div>
+    <MainLayout
+      breadcrumbItems={['首页', "旅游", "国内游"]}
+    >
+      <div className="home-page">
+        <div>
         幻灯片
-      </div>
-    
-      <div>
-        爆款推荐
-      </div>
-
-      <div>
-        新品上市
-      </div>
-
-      <div>
-        国内游推荐
-      </div>
-
-      <div>
-        合作企业
-      </div>
-
-      <footer>
-        footer
-      </footer>
-    </div>
+        </div>
+        <div>
+          爆款推荐
+        </div>
+        <div>
+          新品上市
+        </div>
+        <div>
+          国内游推荐
+        </div>
+        <div>
+          合作企业
+        </div>
+     </div>
+    </MainLayout>
   )
 }
