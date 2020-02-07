7411aae89cfd5c6e27d3a5f8913efc80e6a33e89
Yaduo Liu <yaduol@hotmail.com>
2020-02-06 16:02:05 -0800

    产品详情页面 1. 添加 锚点菜单 2. 添加 html 数据源显示：产品特色，费用，预订须知，用户评价

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { Row, Col, DatePicker } from 'antd';
+import { Row, Col, DatePicker, Anchor, Menu, Typography, Divider } from 'antd';
 import { MainLayout, ProductIntro } from '../../components'
 import { basicInfoMockData } from './mockup'
 const { RangePicker } = DatePicker;
+const { SubMenu } = Menu;
+const { Link } = Anchor;
+const { Title } = Typography;
 
 export const Detail: React.FC = () => {
   
@@ -38,22 +41,64 @@ export const Detail: React.FC = () => {
         </Row>
       </div>
 
-      <div>
-        日期选择
+      {/* 锚点菜单 */}
+      <Anchor style={{marginTop: 20, marginBottom: 20}}>
+        <Menu mode="horizontal">
+          <Menu.Item key="1">
+            <Link href="#product-feature" title="产品特色"></Link>
+          </Menu.Item>
+          <Menu.Item key="3"> 
+            <Link href="#product-fees" title="费用"></Link>
+          </Menu.Item>
+          <Menu.Item key="4"> 
+            <Link href="#product-notes" title="预订须知"></Link>
+          </Menu.Item>
+          <Menu.Item key="5">
+            <Link href="#product-comments" title="用户评价"></Link>
+          </Menu.Item>
+        </Menu>
+      </Anchor>  
+
+      {/* 产品特色 */}
+      <div id="product-feature" style={{background: "white", paddingTop: 20}}>
+        <Divider orientation="center">
+          <Title level={3} >产品特色</Title>
+        </Divider>
+        <div 
+          dangerouslySetInnerHTML={{ __html: basicInfoMockData.features }}
+          style={{margin: 50}}
+        />
       </div>
 
-      <div>
-        商品详情
+      {/* 费用 */}
+      <div id="product-fees" style={{background: "white", marginTop: 20, paddingTop: 20}}>
+        <Divider orientation="center">
+          <Title level={3} >费用</Title>
+        </Divider>
+        <div 
+          dangerouslySetInnerHTML={{ __html: basicInfoMockData.fees }} 
+          style={{margin: 50}}
+        />
       </div>
 
-      <div>
-        商品评价
+      {/* 预订须知 */}
+      <div id="product-notes" style={{background: "white", marginTop: 20, paddingTop: 20}}>
+        <Divider orientation="center">
+          <Title level={3} >预订须知</Title>
+        </Divider>
+        <div 
+          dangerouslySetInnerHTML={{ __html: basicInfoMockData.notes }} 
+          style={{margin: 50}}
+        />
       </div>
 
-      <div>
-        商品评论
+      {/* 商品评价*/}
+      <div id='product-comments' style={{background: "white", marginTop: 20, paddingTop: 20}}>
+        <Divider orientation="center">
+          <Title level={3} >用户评价</Title>
+        </Divider>
       </div>
+
     </MainLayout>
-    
   )
 }
