39ae2574f46eead281ac41e30d9c544bdf2492f2
Yaduo Liu <yaduol@hotmail.com>
2020-02-06 23:15:15 -0800

    添加搜索页面“分类搜索”组件

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { MainLayout } from '../../components'
+import { Divider } from 'antd';
+import { MainLayout, Filter } from '../../components'
 
 export const Search: React.FC = () => {
 
@@ -12,8 +13,14 @@ export const Search: React.FC = () => {
       breadcrumbItems={['首页', "搜索", keywords]}
     >
         {/* 分类 filter*/}
-        <div>
-          分类 filter
+        <div style={{padding: 20, marginTop: 20, background: 'white'}}>
+          <Filter title='出发城市' tags={["北京", "上海", "广州", "深圳"]}/>
+          <Divider dashed />
+          <Filter title='行程天数' tags={["2日", "3日", "4日", "5日", "6日"]}/>
+          <Divider dashed />
+          <Filter title='旅程类型' tags={["跟团游", "自由行", "自驾游", "高端定制"]}/>
+          <Divider dashed />
+          <Filter title='出发时间' tags={["春节", "清明", "劳动节"]}/>
         </div>
 
         {/* 产品列表 */}
